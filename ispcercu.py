d822a76af589d48a02648fa585ac99da309bd1ca
nicoercu <106544022+nicoercu@users.noreply.github.com>
2022-08-12 11:34:26 -0300

    punto 1

---
+++ ispcercu.py
@@ -0,0 +1,6 @@
+dueno = ['28957346',  'Juan',  'Perez', '4789689',  'Belgrano 101']
+telefono = dueno[3]
+dni = dueno[0]
+dni = int(dni)
+if dni > 26000000:
+    print('El numero de telefono es:', telefono)
