62e76565bbd1b3468be91c25dd8f2a05989ffde3
JavierCarranza0101 <javishiva22@gmail.com>
2022-08-13 20:21:10 -0300

    subir archivo Python

---
+++ ejercicioPYTHON.py
@@ -0,0 +1,10 @@
+#    3)  Crear un lista en Python denominado Perro que contenga los siguientes valores:
+#    13,  Puppy,  12/12/2012 , Macho, 123
+
+Perro=[13,  "Puppy",  "12/12/2012" , "Macho", 123]
+
+# Modificar la fecha de nacimiento por 13/12/2012 y reemplazar dni del dueño por 28957346 .
+
+Perro[2]="13/12/2012"
+
+Perro[4]=28957346
